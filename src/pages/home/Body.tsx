bf5b6b03c0b00b7c0feb3d546514c59dd65cf5b2
MadDogOwner <xiaoran@xrgzs.top>
2025-08-06 08:26:39 +0800

    refactor(home): replace VStack with div for layout

--- src/pages/home/Body.tsx
+++ src/pages/home/Body.tsx
@@ -1,4 +1,3 @@
-import { VStack } from "@hope-ui/solid"
 import { Nav } from "./Nav"
 import { Obj } from "./Obj"
 import { Readme } from "./Readme"
@@ -8,15 +7,7 @@ import { Sidebar } from "./Sidebar"
 export const Body = () => {
   return (
     <Container>
-      <VStack
-        class="body"
-        mt="$1"
-        py="$2"
-        px="2%"
-        minH="80vh"
-        w="$full"
-        gap="$4"
-      >
+      <div class="mt-1 flex min-h-[80vh] w-full flex-col gap-4 px-[2%] py-2">
         <Readme files={["header.md", "top.md", "index.md"]} fromMeta="header" />
         <Nav />
         <Obj />
@@ -25,7 +16,7 @@ export const Body = () => {
           fromMeta="readme"
         />
         <Sidebar />
-      </VStack>
+      </div>
     </Container>
   )
 }
